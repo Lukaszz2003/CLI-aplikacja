7342610b330c2e6f5d93e3ee71832b2e86378809
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-10-15 14:22:47 +0200

    last

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { program } = require("commander");
 
 const contacts = require("./contacts");
 
+// funkcja wywołuje metody z pliku contacts.js i przekazuje argumenty
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
@@ -47,6 +48,8 @@ program
   .option("-n, --name <type>", "user name")
   .option("-e, --email <type>", "user email")
   .option("-p, --phone <type>", "user phone");
+
+// parsowanie danych wyjściowych
 
 program.parse(process.argv);
 
